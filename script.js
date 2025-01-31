05d679f09bd7cd370fc0451dae1583d5100cd64a
Nhlanhla Moyo <156192015+nhlexric28@users.noreply.github.com>
2025-01-31 19:00:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ const sketchGallery = document.getElementById('sketch-gallery');
 
 if (portraitGallery && sketchGallery) {
     const portraits = [
-        { src: 'Images/Portraites/art1.jpg', alt: 'Art 1' },
+        { src: 'Images/Portraites/art1.JPG', alt: 'Art 1' },
         { src: 'Images/Portraites/art2.jpg', alt: 'Art 2' },
         { src: 'Images/Portraites/art3.jpg', alt: 'Art 3' },
         { src: 'Images/Portraites/art4.jpg', alt: 'Art 4' },
